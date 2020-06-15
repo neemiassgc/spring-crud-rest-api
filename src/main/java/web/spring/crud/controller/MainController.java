c8ffaaa88497b55413813a52bdb4ff4a5365f55a
sevlasnog <neemiassantos@pm.me>
2020-06-15 18:35:48 -0300

    Update CORS for sevlasnog.github.io domain

--- src/main/java/web/spring/crud/controller/MainController.java
+++ src/main/java/web/spring/crud/controller/MainController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-@CrossOrigin("sevlasnog.github.io")
+@CrossOrigin("https://sevlasnog.github.io")
 public final class MainController implements RestAdapter {
 
     @Autowired
